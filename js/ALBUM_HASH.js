96fb4fdb471ed9bf57cdaf361cf8d594fdbd9335
Phillip Hermans <phermans@splunk.com>
2015-09-30 21:32:54 -0700

    Add node-webkit support

---
+++ js/ALBUM_HASH.js
@@ -0,0 +1,70 @@
+var ALBUM_HASH = [ 
+    { 
+        artist: 'Metallica',
+        album: 'Ride the Lightning'
+    },
+    {
+        artist: 'Primus',
+        album: 'Frizzle Fry'
+    },
+    { 
+        artist: 'Led Zeppelin',
+        album: 'Houses of the Holy'
+    },
+    { 
+        artist: 'Townes Van Zandt',
+        album: 'Delta Mamma Blues'
+    },
+    { 
+        artist: 'Shabazz Palaces',
+        album: 'Lese Majesty'
+    },
+    { 
+        artist: 'Raymond Scott',
+        album: 'Manhattan Research, Inc.'
+    },
+    { 
+        artist: 'Bruce Haack',
+        album: 'Farad: The Electric Voice'
+    },
+    { 
+        artist: 'Sonny Sharrock',
+        album: 'Ask the Ages'
+    },
+    {
+        artist: 'Funkadelic',
+        album: 'Maggot Brain'
+    },
+    {
+        artist: 'Robert Ellis',
+        album: 'Photographs'
+    },
+    {
+        artist: "Sarah Ruth",
+        album: "Words on the Wind"
+    },
+    {
+        artist: 'Sam Cooke',
+        album: 'A Change is Gonna Come'
+    },
+    {
+        artist: 'Zs',
+        album: 'Arms'
+    },
+    {
+        artist: 'AC/DC',
+        album: 'Highway to Hell'
+    },
+    {
+        artist: 'Beastie Boys',
+        album: 'License to Ill'
+    },
+    {
+        artist: 'The Beatles',
+        album: 'The White Album'
+    },
+    {
+        artist: 'King Crimson',
+        album: 'The Court of the Crimson King'
+    }
+];
