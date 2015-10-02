440f482ea912270d46a13baf44fafd640e2ec2b3
Phillip Hermans <phermans@splunk.com>
2015-10-01 17:38:57 -0700

    Remove old junk

--- js/collections/albums.js
+++
@@ -1,6 +0,0 @@
-App.Collections.Albums = Backbone.Collection.extend({
-    initialize: function(){
-    },
-    model: App.Models.Album,
-    url: 'https://c1373283025.web.cddbp.net/webapi/json/1.0/'
-});
